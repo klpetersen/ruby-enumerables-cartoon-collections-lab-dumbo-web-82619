0789a4681715bf419353887fd7026557719ff2c4
Kayla Petersen <petersen1994@gmail.com>
2019-08-20 16:11:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,8 @@ def roll_call_dwarves(array)
   array.each_with_index{|el, i| puts "#{i+1} #{el}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  array.collect(|planet| "#{planet.capitalize}!")
   
 end
 
